4e3995311d3b7952cfc45b5494a44867c15e5e7e
billybonks <sebastienstettler@gmail.com>
2025-02-23 19:55:32 +0800

    COULDNT FIND EQUIVALENTS
     redis-tools
    No match for argument: gir1.2-gtop-2.0
    No match for argument: gir1.2-clutter-1.0
    
    development-tools solved these
    bison covered
     libssl-dev caled  openssl-devel

--- install/terminal/libraries.sh
+++ install/terminal/libraries.sh
@@ -1,5 +1,7 @@
 sudo dnf install -y \
-  build-essential pkg-config autoconf bison clang rustc \
-  libssl-dev libreadline-dev zlib1g-dev libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev libjemalloc2 \
-  libvips imagemagick libmagickwand-dev mupdf mupdf-tools gir1.2-gtop-2.0 gir1.2-clutter-1.0 \
-  redis-tools sqlite3 libsqlite3-0 libmysqlclient-dev libpq-dev postgresql-client postgresql-client-common
+  pkg-config autoconf  clang rustc \
+  readline-devel zlib-devel libyaml-devel  ncurses  libffi-devel libgdbm-devel jemalloc \
+  vips ImageMagick ImageMagick-devel ImageMagick-c++ mupdf \
+  libpq libpq-devel sqlite sqlite-devel mysql mysql-devel
+  
+sudo dnf group install development-tools
