249150825ee83eeb2f89fa6b76a914caf20da8bf
billybonks <sebastienstettler@gmail.com>
2025-02-23 22:50:13 +0800

    dont do desktop yet

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ source ~/.local/share/omakub/install/terminal.sh
 
 if $RUNNING_GNOME; then
   # Install desktop tools and tweaks
-  source ~/.local/share/omakub/install/desktop.sh
+  # source ~/.local/share/omakub/install/desktop.sh
 
   # Revert to normal idle and lock settings
   gsettings set org.gnome.desktop.screensaver lock-enabled true
